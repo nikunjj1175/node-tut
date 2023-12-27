ddc357b7323c0e266c3c6e9c1c7e4d6ec6dc69a9
nikunjj1175 <nikunjkorat117@gmail.com>
2023-12-27 11:07:16 +0530

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-hello 
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) =>{
+    res.send('nikunjkorat')
+})
+
+app.get('/login', (req, res) =>{
+    res.send('<h1>please login at node-tut</h1>')
+})
+
+app.listen(process.env.port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
